81add75241fc3db377c6b4c6b02279bcca9af3a7
Chandrashekhar Satav <cpsatav@gmail.com>
2025-07-21 17:17:34 +0530

    Folder Structure and setup.py updated

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages    # Import setuptools for creating a Python package
+
+setup(
+    name="document_portal",
+    author="Chandrashekhar Satav",
+    version="0.1",
+    packages=find_packages(),
+  
+)
