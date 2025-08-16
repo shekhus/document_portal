8c855fafea0dd635acfe8d558b8a38d1210178f4
Chandrashekhar Satav <cpsatav@gmail.com>
2025-08-16 10:12:33 +0530

    prepared code skeleton for mult_document_chat

---
+++ src/multi_document_chat/data_ingestion.py
@@ -0,0 +1,11 @@
+class DocumentIngestor:
+    def __init__(self, ):
+        pass
+
+    def ingest_files(self):
+        # Logic to ingest the document
+        pass
+
+    def _create_document_loader(self, documents):
+        # Logic to create a document from the file path
+        pass
---
+++ src/multi_document_chat/retrieval.py
@@ -0,0 +1,22 @@
+class ConversationalRAG:
+    def __init__(self, ):
+        pass
+    def load_retriver_from_faiss(self):
+        # Logic to load the retriever from FAISS index
+        pass
+    def invoke(self):
+        # Logic to invoke the retriever
+        pass
+    
+    def _load_llm ():
+        pass
+    
+    
+    @staticmethod
+    def _format_docs(docs):
+        # Logic to format the documents
+        pass        
+
+    def _build_Icel_chain(self):
+        # Logic to build the Icel chain
+        pass
